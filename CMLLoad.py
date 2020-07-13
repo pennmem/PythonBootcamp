66d37d5b04c3145874cd9499eeaba6959aff3e47
Ryan A. Colyer <ryancolyer@yahoo.com>
2020-07-13 19:15:59 -0400

    Fix PTSA time axis coordinates.

--- CMLLoad.py
+++ CMLLoad.py
@@ -6,10 +6,10 @@
 # use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -119,7 +119,7 @@ class CMLLoad():
       st = 0
     else:
       st = ev_start
-    en = st + (data.shape[-1]-1)*sr/1000.
+    en = st + (data.shape[-1]-1)*1000./sr
     time = np.linspace(st, en, data.shape[-1])
 
     coords = {'channel':[str(c) for c in channels.label], 'time':time}
